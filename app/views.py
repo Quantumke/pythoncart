db7437a5421ab15233d233ff7880df804557e6bf
quantumke <nguruben@gmail.com>
2016-09-19 07:32:02 +0300

    add to cart controller

--- app/views.py
+++ app/views.py
@@ -9,3 +9,28 @@ def home(request):
 	return render_to_response('index.html', {
 		'items':items
 		})
+
+#@login_required
+def addcart(request, slug):
+	session_id="nguruben@gmail.com"
+	print(slug)
+	slug=slug
+	item=Item.objects.get(item_link=slug)
+	items=cart.objects.filter(cart_session_id=session_id)
+	a=items.aggregate(Sum('cart_item_price'))
+	quantity=items.count()
+	item_quantity=quantity+1
+	cart_items={}
+	cart_items['cart_item_name']=item.item_name
+	cart_items['cart_item_description']=item.item_description
+	cart_items['cart_item_price']=str(item.item_price)
+	cart_items['cart_session_id']=session_id
+	cart_items['cart_slug']=session_id
+	cart_items['quantity']=str(item_quantity)
+	#print (cart_items)
+	save=cart(**cart_items)
+	#save=state_cart(cart_item_name=item.item_name,cart_item_description=item.item_description, cart_item_price=item.item_price,cart_session_id=session_id)
+	#save.save()
+	return render_to_response('index.html',{
+		'slug':slug
+		})
